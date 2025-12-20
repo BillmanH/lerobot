eee8cc753d48375b5e45fed1c4f1955c0da30bf0
billmanh <william.jeffrey.harding@gmail.com>
2025-12-19 17:29:16 -0800

    updated 02

--- 02_setup_motors_leader.py
+++ 02_setup_motors_leader.py
@@ -1,11 +1,18 @@
+import sys
+from pathlib import Path
+
+# Add the src directory to Python path
+sys.path.insert(0, str(Path(__file__).parent / "src"))
+
 from lerobot.teleoperators.so101_leader import SO101Leader, SO101LeaderConfig
+
 import yaml
 
 with open("local_configurations.yaml", "r") as f:
     local_configurations = yaml.safe_load(f)
 
 config = SO101LeaderConfig(
-    port=local_configurations['leader_port'],
+    port=local_configurations["leader_port"],
     id="my_awesome_leader_arm",
 )
 leader = SO101Leader(config)
